23bedd5e88f1905c045a255be797eb87470dc2fa
Saloni Agrawal <saloniagrawal432@gmail.com>
2026-04-25 09:11:17 +0530

    Fix database URL to use environment variable

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:password@localhost:5432/usersdb"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:password@localhost:5432/usersdb")
 
 engine = create_engine(DATABASE_URL)
 
