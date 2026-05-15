8e70b487c584295023c3730dbe8b6be2c8f464ad
Saloni Agrawal <saloniagrawal432@gmail.com>
2026-05-15 09:41:47 +0530

    Add redis

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:password@localhost:5432/usersdb")
+DATABASE_URL = "postgresql://postgres:zCbfDRKdzwdYJuFVNaaAXFueXoPXCIRo@shuttle.proxy.rlwy.net:13736/railway"
 
 engine = create_engine(DATABASE_URL)
 
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import Session
 from app.core.database import SessionLocal
 from app.crud import user_crud
 from app.schemas.user_schemas import UserCreate, UserUpdate
+import redis, json
 
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 router = APIRouter(prefix="/users", tags=["Users"])
 
 def get_db():
@@ -15,20 +17,72 @@ def get_db():
 
 @router.post("/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    return user_crud.create_user(db, user)
+    user = user_crud.create_user(db, user)
+    redis_client.delete("users:all")
+    return user
 
 @router.get("/")
 def get_users(db: Session = Depends(get_db)):
-    return user_crud.get_users(db)
+    try:
+       cached_users = redis_client.get("users:all")
+    except Exception:
+        cached_users = None
+    
+    if cached_users:
+        return {
+            "cached": True,
+            "data": json.loads(cached_users)
+        }
+    
+    users = user_crud.get_users(db)
+    users_data = [{"id": user.id, "name": user.name, "email": user.email} for user in users]
+    redis_client.setex("users:all", 3600, json.dumps(users_data))
+    return {
+        "cached": False,
+        "data": users_data
+    }
 
 @router.get("/{user_id}")
 def get_user(user_id: int, db: Session = Depends(get_db)):
-    return user_crud.get_user(db, user_id)
+    try:
+         cached_user = redis_client.get(f"user:{user_id}")
+    except Exception:
+        cached_user = None
+
+    if cached_user:
+        return {
+            "cached": True,
+            "data": json.loads(cached_user)
+        }
+
+    user = user_crud.get_user(db, user_id)
+
+    if not user:
+        return {"message": "User not found"}
+
+    user_data = {
+        "id": user.id,
+        "name": user.name,
+        "email": user.email
+    }
+
+    redis_client.setex(
+        f"user:{user_id}",
+        3600,
+        json.dumps(user_data)
+    )
+
+    return {
+        "cached": False,
+        "data": user_data
+    }
 
 @router.put("/{user_id}")
 def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    redis_client.delete("users:all")
     return user_crud.update_user(db, user_id, user)
 
 @router.delete("/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
+    redis_client.delete("users:all")
     return user_crud.delete_user(db, user_id)
